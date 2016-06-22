b23939ecb11c5f8d3cef9f16c903afaf266f8260
Vincent S. Cojot <vincent@cojot.name>
2016-06-22 14:46:50 -0400

    Make the default user unprivileged

--- ubuntuWithNvidiaDriver/run.sh
+++ ubuntuWithNvidiaDriver/run.sh
@@ -3,4 +3,4 @@
 COMMAND=/bin/bash
 
 xhost + # allow connections to X server
-docker run --privileged -e "DISPLAY=unix:0.0" -v="/tmp/.X11-unix:/tmp/.X11-unix:rw"  -i -t ubuntu_with_nvidia_driver $COMMAND
+docker run --privileged -e "DISPLAY=unix:0.0" -v="/tmp/.X11-unix:/tmp/.X11-unix:rw"  -i -t ubuntu_with_nvidia_driver sudo su - user1
