982b74f4487df75e3984cee9df894159220279e0
Vincent S. Cojot <vincent@cojot.name>
2017-05-09 16:29:16 -0400

    Revamp and fixes...

--- ubuntuWithNvidiaDriver/build.sh
+++ ubuntuWithNvidiaDriver/build.sh
@@ -5,9 +5,9 @@ NVIDIA_VERSION=$(/sbin/modinfo -F version nvidia)
 # path to nvidia driver
 NVIDIA_DRIVER=NVIDIA-Linux-x86_64-${NVIDIA_VERSION}.run
 if [ -f ${NVIDIA_DRIVER} ]; then
-	cp ${NVIDIA_DRIVER} NVIDIA-DRIVER.run
+	echo "Using existing ${NVIDIA_DRIVER}..."
 else
-	wget -O ${NVIDIA_DRIVER} http://us.download.nvidia.com/XFree86/Linux-x86_64/${NVIDIA_VERSION}/${NVIDIA_DRIVER} && \
-	cp ${NVIDIA_DRIVER} NVIDIA-DRIVER.run || exit 127
+	wget -O ${NVIDIA_DRIVER} http://us.download.nvidia.com/XFree86/Linux-x86_64/${NVIDIA_VERSION}/${NVIDIA_DRIVER} || exit 127
 fi
+/bin/cp -f ${NVIDIA_DRIVER} NVIDIA-DRIVER.run
 sudo docker build -t ${IMAGE_NAME} .
--- ubuntuWithNvidiaDriver/run.sh
+++ ubuntuWithNvidiaDriver/run.sh
@@ -8,7 +8,8 @@ if [ "x${DISPLAY}" = "x" ]; then
 		-v="/var/tmp:/var/tmp:rw" \
 		-i -t docker3d sudo su - worker
 else
-	xhost + # allow connections to X server
+	#xhost + # allow connections to X server
+	xhost local:root
 	docker run --privileged \
 		-e "DISPLAY=unix:$(echo ${DISPLAY}|cut -d: -f2)" \
 		-v="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
