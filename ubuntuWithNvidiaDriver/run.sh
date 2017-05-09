cd261d16e2407fc1c416e18ae91039edc14bf9fa
Vincent S. Cojot <vincent@cojot.name>
2017-05-09 16:41:53 -0400

    add user...

--- ubuntuWithNvidiaDriver/run.sh
+++ ubuntuWithNvidiaDriver/run.sh
@@ -6,7 +6,7 @@ if [ "x${DISPLAY}" = "x" ]; then
 	docker run --privileged \
 		-v="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
 		-v="/var/tmp:/var/tmp:rw" \
-		-i -t docker3d sudo su - worker
+		-i -t -u worker docker3d
 else
 	#xhost + # allow connections to X server
 	xhost local:root
@@ -14,5 +14,5 @@ else
 		-e "DISPLAY=unix:$(echo ${DISPLAY}|cut -d: -f2)" \
 		-v="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
 		-v="/var/tmp:/var/tmp:rw" \
-		-i -t docker3d sudo su - worker
+		-i -t -u worker docker3d
 fi
