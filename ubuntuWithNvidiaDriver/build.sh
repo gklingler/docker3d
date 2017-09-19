f964d39397ae5a0e0296d5bc4ec14370caf91370
Vincent S. Cojot <vincent@cojot.name>
2017-09-18 22:13:04 -0400

    Initial working version...

--- ubuntuWithNvidiaDriver/build.sh
+++ ubuntuWithNvidiaDriver/build.sh
@@ -10,4 +10,5 @@ else
 	wget -O ${NVIDIA_DRIVER} http://us.download.nvidia.com/XFree86/Linux-x86_64/${NVIDIA_VERSION}/${NVIDIA_DRIVER} || exit 127
 fi
 /bin/cp -f ${NVIDIA_DRIVER} NVIDIA-DRIVER.run
-sudo docker build -t ${IMAGE_NAME} .
+######sudo docker build -t ${IMAGE_NAME} .
+docker build -t ${IMAGE_NAME} .
