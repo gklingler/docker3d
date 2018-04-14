036ba40fc62ba6b7cd87eb350d69669bea8f5a93
Vincent S. Cojot <vincent@cojot.name>
2018-04-13 22:52:06 -0400

    Update_on_el_7.5

--- ubuntuWithNvidiaDriver/run.sh
+++ ubuntuWithNvidiaDriver/run.sh
@@ -10,7 +10,7 @@ if [ "x${DISPLAY}" = "x" ]; then
 else
 	#xhost + # allow connections to X server
 	xhost local:root
-	docker run --privileged \
+	docker run --privileged --rm \
 		-e "DISPLAY=unix:$(echo ${DISPLAY}|cut -d: -f2)" \
 		-v="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
 		-v="/var/tmp:/var/tmp:rw" \
