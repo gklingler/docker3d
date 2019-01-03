a3b0e3167f807e35864ab00e7cc96bf1462aa560
Vikas Dhiman <dhiman@umich.edu>
2019-01-03 13:29:36 -0500

    need nvidia driver on the docker to run opengl

--- ubuntuWithNvidiaDriver/run.sh
+++ ubuntuWithNvidiaDriver/run.sh
@@ -2,5 +2,12 @@
 
 COMMAND=/bin/bash
 
-xhost + # allow connections to X server
-docker run --privileged -e "DISPLAY=unix:0.0" -v="/tmp/.X11-unix:/tmp/.X11-unix:rw"  -i -t ubuntu_with_nvidia_driver $COMMAND
+# allows docker to connect to x11 server at the host
+XAUTH=/tmp/.docker.xauth
+touch $XAUTH
+xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
+
+
+docker run --privileged -e "DISPLAY=unix:0.0" -v="/tmp/.X11-unix:/tmp/.X11-unix:rw"  \
+       -e "XAUTHORITY=$XAUTH" -v="$XAUTH:$XAUTH:rw" \
+       -it ubuntu_with_nvidia_driver $COMMAND
